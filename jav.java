11d49ad8820a514782947c110d2f81dbcef239fd
MxNobody <ductam1428@gmail.com>
2025-05-28 21:03:58 +0900

    wooohoaaaaaaaaaa

---
+++ jav.java
@@ -0,0 +1,15 @@
+class A{
+    private int x=1;
+
+    public int getX(){
+        return x;
+    }
+    
+    public static void main(String[] args) {    
+
+        A a = new A();
+        System.out.println(a.getX());
+    }
+
+}
+
